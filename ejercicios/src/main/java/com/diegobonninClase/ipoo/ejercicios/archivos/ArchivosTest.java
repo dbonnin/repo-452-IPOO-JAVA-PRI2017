f368f3ee5e0235f8b374d6d3fdbf4dab62a9678a
dbonnin <dbonnin@visionbanco.com>
2017-11-16 16:56:09 -0300

    Ejercicios de archivos

---
+++ ejercicios/src/main/java/com/diegobonninClase/ipoo/ejercicios/archivos/ArchivosTest.java
@@ -0,0 +1,71 @@
+package com.diegobonninClase.ipoo.ejercicios.archivos;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class ArchivosTest {
+	
+	private String base="C:\\archivos";
+	private Archivos a;
+	private ConversorObjetos co;
+	
+	@Before
+	public void iniciar(){
+		a=new Archivos();
+		co=new ConversorObjetos();
+	}
+	
+	
+	// @Test
+	public void testObjeto(){
+		
+		Cuenta c=new Cuenta();
+		String json;
+		
+		json = co.convertirAJson(c);
+		
+		a.escribirArchivo(base + "\\persona.txt",  json);		
+		System.out.println(a.leerArchivo(base + "\\persona.txt"));
+		
+	}		
+	
+	// @Test
+	public void testCopiar(){
+		
+		Archivos a=new Archivos();
+		a.copiar(
+				base + "\\origen.txt", 
+				base + "\\destino.txt"
+		);
+		
+	}
+	
+	@Test
+	public void testSplit(){
+		
+		String entrada=a.leerArchivo(base + "\\cuentas.txt");
+		
+		String[] lineas=entrada.split("\n");
+		
+		for(int i=0; i<lineas.length; i++){
+			
+			System.out.print(lineas[i]);
+			
+			String[] columnas=lineas[i].split("\\|");
+			
+			for(int c=0; c<columnas.length; c++){
+			
+				System.out.print(columnas[c] + " ");
+				
+			}
+			
+			System.out.println();
+			
+		}
+		
+	}		
+	
+	
+	
+
+}
---
+++ ejercicios/src/main/java/com/diegobonninClase/ipoo/ejercicios/archivos/ConversorObjetosTest.java
@@ -0,0 +1,38 @@
+package com.diegobonninClase.ipoo.ejercicios.archivos;
+
+import java.util.List;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class ConversorObjetosTest {
+	
+	private List<Cuenta> cuenta;
+	private ConversorObjetos co=new ConversorObjetos();
+
+	@Before
+	public void setUp() throws Exception {
+//		clientes=new ArrayList<Cliente>();
+//		clientes.add(new Cliente(1l));
+//		clientes.add(new Cliente(2l));
+//		clientes.add(new Cliente(3l));
+	}
+
+	@Test
+	public void testConvertirAJson() {
+	}
+
+	@Test
+	public void testConvertirAObjeto() {
+	}
+	
+	@Test
+	public void test(){
+//		String json=co.convertirAJson(clientes);
+//		System.out.println(json);
+//		List<Cliente> l=(List<Cliente>)co.convertirAObjeto(json, new TypeReference<List<Cliente>>(){});
+//		String jsonl=co.convertirAJson(clientes);
+//		System.out.println(jsonl);
+	}
+
+}
