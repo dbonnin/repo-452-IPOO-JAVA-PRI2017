f4a0cc68b34c024753990fa40df4ff7249190ae9
dbonnin <dbonnin@visionbanco.com>
2017-12-16 15:00:55 -0300

    Script de creación de DB

--- cajero/src/main/resources/script.sql
+++ cajero/src/main/resources/script.sql
@@ -6,68 +6,142 @@ drop table cuentas;
 drop table clientes;
 drop table accesos;
 
-create table accesos(
-	id_acceso serial primary key,
-	fecha_hora_inicio timestamp,
-	fecha_hora_fin timestamp,
-	ip varchar(128)
+
+-- DROP TABLE cajeros;
+
+CREATE TABLE cajeros
+(
+  id_cajero serial NOT NULL,
+  nombre character varying(255),
+  direccion text,
+  CONSTRAINT cajeros_pkey PRIMARY KEY (id_cajero)
+);
+ 
+-- Table: clientes
+
+-- DROP TABLE clientes;
+
+CREATE TABLE clientes
+(
+  id_cliente serial NOT NULL,
+  nro_doc character varying(128),
+  tipo_doc character varying(64),
+  nombre character varying(255),
+  fecha_nacimiento date,
+  password character varying(128),
+  direccion text,
+  telefono character varying(64),
+  CONSTRAINT clientes_pkey PRIMARY KEY (id_cliente),
+  CONSTRAINT clientes_nro_doc_tipo_doc_key UNIQUE (nro_doc, tipo_doc)
 );
 
-create table clientes(
-	id_cliente serial primary key,
-	nro_doc varchar(128),
-	tipo_doc varchar(64),
-	nombre varchar(255),
-	fecha_nacimiento date,
-	password varchar(128),
-	direccion text,
-	nro_telefono varchar(64),
-	unique(nro_doc, tipo_doc)
+-- Table: accesos
+
+-- DROP TABLE accesos;
+
+CREATE TABLE accesos
+(
+  id_acceso serial NOT NULL,
+  fecha_hora_inicio timestamp without time zone,
+  fecha_hora_fin timestamp without time zone,
+  id_cajero bigint,
+  id_cliente bigint,
+  CONSTRAINT accesos_pkey PRIMARY KEY (id_acceso),
+  CONSTRAINT accesos_id_cajero_fkey FOREIGN KEY (id_cajero)
+      REFERENCES cajeros (id_cajero) MATCH SIMPLE
+      ON UPDATE NO ACTION ON DELETE NO ACTION,
+  CONSTRAINT accesos_id_cliente_fkey FOREIGN KEY (id_cliente)
+      REFERENCES clientes (id_cliente) MATCH SIMPLE
+      ON UPDATE NO ACTION ON DELETE NO ACTION
+); 
+
+-- Table: monedas
+
+-- DROP TABLE monedas;
+
+CREATE TABLE monedas
+(
+  id_moneda serial NOT NULL,
+  nombre character varying(32),
+  CONSTRAINT monedas_pkey PRIMARY KEY (id_moneda)
+);
+  
+-- Table: cuentas
+
+-- DROP TABLE cuentas;
+
+CREATE TABLE cuentas
+(
+  nro_cuenta character varying(32) NOT NULL,
+  tipo character(2),
+  denominacion character varying(255),
+  saldo_disponible numeric(18,2),
+  saldo_a_confirmar numeric(18,2),
+  id_moneda bigint,
+  CONSTRAINT cuentas_pkey PRIMARY KEY (nro_cuenta),
+  CONSTRAINT cuentas_id_moneda_fkey FOREIGN KEY (id_moneda)
+      REFERENCES monedas (id_moneda) MATCH SIMPLE
+      ON UPDATE NO ACTION ON DELETE NO ACTION
+);
+  
+ Table: operaciones
+
+-- DROP TABLE operaciones;
+
+CREATE TABLE operaciones
+(
+  nro_operacion serial NOT NULL,
+  fecha_hora timestamp without time zone,
+  id_cajero bigint,
+  id_acceso bigint,
+  estado character varying(32),
+  mensaje text,
+  tipo character varying(64),
+  CONSTRAINT operaciones_pkey PRIMARY KEY (nro_operacion),
+  CONSTRAINT operaciones_id_acceso_fkey FOREIGN KEY (id_acceso)
+      REFERENCES accesos (id_acceso) MATCH SIMPLE
+      ON UPDATE NO ACTION ON DELETE NO ACTION,
+  CONSTRAINT operaciones_id_cajero_fkey FOREIGN KEY (id_cajero)
+      REFERENCES cajeros (id_cajero) MATCH SIMPLE
+      ON UPDATE NO ACTION ON DELETE NO ACTION
+); 
+
+-- Table: transacciones
+
+-- DROP TABLE transacciones;
+
+CREATE TABLE transacciones
+(
+  nro_operacion bigint NOT NULL,
+  importe numeric(18,2),
+  id_moneda integer,
+  CONSTRAINT transacciones_pkey PRIMARY KEY (nro_operacion),
+  CONSTRAINT transacciones_id_moneda_fkey FOREIGN KEY (id_moneda)
+      REFERENCES monedas (id_moneda) MATCH SIMPLE
+      ON UPDATE NO ACTION ON DELETE NO ACTION,
+  CONSTRAINT transacciones_nro_operacion_fkey FOREIGN KEY (nro_operacion)
+      REFERENCES operaciones (nro_operacion) MATCH SIMPLE
+      ON UPDATE NO ACTION ON DELETE NO ACTION
 );
 
-create table cuentas(
-	nro_cuenta varchar(32) primary key,
-	tipo char(2),
-	denominacion varchar(255),
-	saldo_disponible numeric(18, 2),
-	saldo_a_confirmar numeric(18, 2)
-);
 
-create table cuentas_clientes(
-	id_cliente bigint,
-	nro_cuenta varchar(32),
-	primary key(id_cliente, nro_cuenta)
-)
+-- Table: transferencias
 
-create table monedas(
-	id_moneda serial primary key,
-	nombre varchar(32)
-)
+-- DROP TABLE transferencias;
 
-create table cajeros(
-	id_cajero serial primary key,
-	nombre varchar(255),
-	direccion text
-);
-
-create table operaciones(
-	nro_operacion serial primary key,
-	fecha_hora timestamp,
-	id_cajero bigint references cajeros(id_cajero),
-	id_acceso bigint references accesos(id_acceso),
-	estado varchar(32),
-	mensaje text
-);
-
-
-create table transacciones(
-	nro_operacion bigint primary key references operaciones(nro_operacion),
-	importe numeric(18, 2),
-	id_moneda int references monedas(id_moneda)
-);
-
-create table transferencias(
-	nro_operacion bigint primary key references transacciones(nro_operacion),
-	nro_cuenta_origen varchar(32) references cuentas(nro_cuenta),
-	nro_cuenta_destino varchar(32) references cuentas(nro_cuenta)
-);
+CREATE TABLE transferencias
+(
+  nro_operacion bigint NOT NULL,
+  nro_cuenta_origen character varying(32),
+  nro_cuenta_destino character varying(32),
+  CONSTRAINT transferencias_pkey PRIMARY KEY (nro_operacion),
+  CONSTRAINT transferencias_nro_cuenta_destino_fkey FOREIGN KEY (nro_cuenta_destino)
+      REFERENCES cuentas (nro_cuenta) MATCH SIMPLE
+      ON UPDATE NO ACTION ON DELETE NO ACTION,
+  CONSTRAINT transferencias_nro_cuenta_origen_fkey FOREIGN KEY (nro_cuenta_origen)
+      REFERENCES cuentas (nro_cuenta) MATCH SIMPLE
+      ON UPDATE NO ACTION ON DELETE NO ACTION,
+  CONSTRAINT transferencias_nro_operacion_fkey FOREIGN KEY (nro_operacion)
+      REFERENCES transacciones (nro_operacion) MATCH SIMPLE
+      ON UPDATE NO ACTION ON DELETE NO ACTION
+););
